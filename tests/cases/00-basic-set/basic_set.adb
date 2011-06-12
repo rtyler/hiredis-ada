93e16c259b07ff92432efd1f0e0c314e26b3a343
R. Tyler Croy <tyler@monkeypox.org>
2011-06-11 17:17:32 -0700

    Add empty test program for basic-set

---
+++ tests/cases/00-basic-set/basic_set.adb
@@ -0,0 +1,13 @@
+--
+--  Test program to execute a `Redis.Set` operation
+--
+
+with Ada.Command_Line,
+     Ada.Text_IO;
+
+use Ada.Text_IO;
+
+procedure Basic_Set is
+begin
+    Put_Line("Basic_Set");
+end Basic_Set;
