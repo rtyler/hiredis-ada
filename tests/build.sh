86d34698cb4a0acea2c9040f373b381da035233c
R. Tyler Croy <tyler@monkeypox.org>
2011-06-26 17:19:41 -0700

    Properly fail out if the building of a particular test case is incorrect

--- tests/build.sh
+++ tests/build.sh
@@ -2,9 +2,18 @@
 
 OBJDIR="${PWD}/obj/tests"
 
+source "tests/functions.sh"
+
 for f in $(find tests -iname "*.adb"); do
     FILE=$(echo $f | cut -d / -f 4);
     DIR=$(echo $f | cut -d / -f -3);
 
     (cd $DIR && gnatmake -gnat05 -L${OBJDIR}/../../build -aI${OBJDIR}/../../src -D $OBJDIR $FILE -largs -lredisada)
+
+    if [ $? -ne 0 ]; then
+        echo
+        redout "FAILED TO BUILD ${FILE}"
+        echo
+        exit 1
+    fi
 done
